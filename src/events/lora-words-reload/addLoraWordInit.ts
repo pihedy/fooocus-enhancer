129cab0821a45f66335b83f212cdf2898a69571b
pihedy <pihedy@gmail.com>
2025-03-28 06:35:00 +0100

    Fix LoRA trigger words container loading issue.

--- src/events/lora-words-reload/addLoraWordInit.ts
+++ src/events/lora-words-reload/addLoraWordInit.ts
@@ -6,27 +6,29 @@ import { gradioApp } from "@/utils/gradioApp";
 import { LoraDropdownElement } from "@/classes/Observers/LoraDropdownElement";
 
 document.addEventListener('fooocus-enhancer-lora-words-reload', () => {
-    gradioApp().find('div.lora_model')?.forEach((Element: Element) => {
-        let Component = Element.closest('div.form')?.parentElement;
-        
-        if (Component === null || typeof Component === 'undefined') {
-            return;
-        }
-
-        let modelId = Component?.getAttribute('id');
-
-        if (modelId === null || modelId === undefined) {
-            return;
-        }
-
-        let inputs = Element.getElementsByTagName('input');
-
-        if (inputs.length <= 0) {
-            return;
-        }
-
-        let Input = new LoraDropdownElement(inputs[0]);
-
-        Input.forcedUpdate();
-    });
+    setTimeout(() => {
+        gradioApp().find('div.lora_model')?.forEach((Element: Element) => {
+            let Component = Element.closest('div.form')?.parentElement;
+            
+            if (Component === null || typeof Component === 'undefined') {
+                return;
+            }
+    
+            let modelId = Component?.getAttribute('id');
+    
+            if (modelId === null || modelId === undefined) {
+                return;
+            }
+    
+            let inputs = Element.getElementsByTagName('input');
+    
+            if (inputs.length <= 0) {
+                return;
+            }
+    
+            let Input = new LoraDropdownElement(inputs[0]);
+    
+            Input.forcedUpdate();
+        });
+    }, 300);
 });
